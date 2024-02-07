f105021783165900fc42eb6fbf4e59e77196f9d9
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2024-02-08 00:34:06 +0200

    Add esbuild build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ToastContainer />
         <Esm />
         <Cjs />
